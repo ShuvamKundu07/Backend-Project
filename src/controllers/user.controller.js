621c536d10f8a667c23b6d8f9239032cca7ff6b0
Shuvam <kundushibam7@gmail.com>
2026-06-08 20:56:28 +0530

    small corrections

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -242,7 +242,7 @@ const changeCurrentPassword = asyncHandler(async (req, res)=>{
 const getCurrentuser = asyncHandler(async (req, res)=>{
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res)=>{
@@ -252,7 +252,7 @@ const updateAccountDetails = asyncHandler(async (req, res)=>{
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = await User.findByIdAndUpdate( // await add korechi
+    const user = await User.findByIdAndUpdate( 
         req.user._id,
         {
             $set: {
